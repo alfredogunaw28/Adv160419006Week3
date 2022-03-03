f22d50124a1f89986c2937ce1ef35c650ebc250d
s160419006 <s160419006@student.ubaya.ac.id>
2022-03-03 19:58:19 +0700

    homework commit

--- app/src/main/java/com/jitusolution/adv160419006week4/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/jitusolution/adv160419006week4/viewmodel/DetailViewModel.kt
@@ -7,7 +7,7 @@ import com.jitusolution.adv160419006week4.model.Student
 class DetailViewModel: ViewModel()  {
     val studentLD = MutableLiveData<Student>()
     fun fetch() {
-        val student1 = Student("07-6347576","Sibley","2001/06/22","2635475556","http://dummyimage.com/201x100.png/cc0000/ffffff")
+        val student1 = Student("07-6345676","Sibley","2001/06/22","2635475556","http://dummyimage.com/201x100.png/cc0000/ffffff")
         studentLD.value = student1
     }
 }
